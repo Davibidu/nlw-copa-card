27c618e2d35a20c2c25aacd1da2dbbe65fe30076
devdavi <davibidu2020@gmail.com>
2022-10-27 15:31:22 -0300

    teste0.1

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let isExplorer = true
+let isIgnite = false
 function changeCard(event) {
- const card = event.currentTarget
- const bg = isExplorer
-   ? "ignite"
-   : "explorer"
-isExplorer = !isExplorer
-card.style.backgroundImage = `url(./assets/${bg}.svg)`
+  const card = event.currentTarget
+  const bg = isIgnite ? "explorer" : "ignite"
+  isIgnite = !isIgnite
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
 }
